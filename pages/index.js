5918304fbb3904849c319b9ea80d59a3ad0d5d15
bmedrano011 <51801134+bmedrano011@users.noreply.github.com>
2023-01-02 09:12:07 -0500

    feat: custom hook and display

--- pages/index.js
+++ pages/index.js
@@ -1,44 +1,126 @@
+import {
+  Button,
+  Grid,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from "@mui/material";
 import React from "react";
-import Container from "@mui/material/Container";
-import Box from "@mui/material/Box";
+import useData from "../src/components/useData";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+import ModeEditOutlineOutlinedIcon from "@mui/icons-material/ModeEditOutlineOutlined";
 import Copyright from "../src/Copyright";
-import StickyTable from "../src/components/Table/index.js";
-import useFetchData from "../src/components/useFetchData";
-
-const tableHeaders = [
-  { id: "id", label: "ID", sticky: true, disabledSorting: false },
-  { id: "email", label: "Email", sticky: true, disabledSorting: false },
-  { id: "name", label: "Name", sticky: true, disabledSorting: false },
-  { id: "country", label: "Country", sticky: false, disabledSorting: false },
-  {
-    id: "ip_address",
-    label: "IP Address",
-    sticky: false,
-    disabledSorting: true,
-  },
-  { id: "split", label: "Split Status", sticky: false, disabledSorting: true },
-  { id: "currency", label: "Currency", sticky: false, disabledSorting: false },
-  { id: "ein", label: "EIN", sticky: false, disabledSorting: true },
-  { id: "md5", label: "MD5 Hash", sticky: false, disabledSorting: false },
-  { id: "notes", label: "Notes", sticky: false, disabledSorting: true },
-];
 
 export default function Index() {
-  const { isLoading, apiData, error } = useFetchData("");
+  const {
+    isLoading,
+    returnData,
+    error,
+    getData,
+    createData,
+    updateData,
+    deleteData,
+  } = useData("posts");
+
+  const handleAdd = () => {
+    //Simulate creation post
+    let post = {
+      title: "Test Post",
+      body: "Test Creation Post",
+      userId: Math.max(...returnData.map((d) => d.userId)) + 1,
+    };
+    createData("posts", post);
+  };
+
+  const handleUpdate = (post) => {
+    updateData(`posts/${post.id}`, post);
+  };
+  const handleRemove = (post) => {
+    deleteData(`posts/${post.id}`, post.id);
+  };
 
   return (
     <>
-      {isLoading && <span>Loading.....</span>}
-      {!isLoading && error ? (
-        <span>Error in fetching data ...</span>
+      {error ? <p>Error fetching data {error}</p> : null}
+      {isLoading ? (
+        <p>Data Loading...</p>
       ) : (
         <>
-          <Container maxWidth="xl">
-            <Box sx={{ my: 4 }}>
-              <StickyTable data={apiData} tableHeaders={tableHeaders} />
-              <Copyright />
-            </Box>
-          </Container>
+          <Grid container direction="column" m={1} spacing={2}>
+            <Grid item>
+              <Typography variant="h1" sx={{ fontSize: 26, fontWeight: 500 }}>
+                POSTS
+              </Typography>
+            </Grid>
+            <Grid item>Post Count: {returnData.length}</Grid>
+            <Grid item container spacing={2}>
+              <Grid item>
+                <Button variant="outlined" onClick={() => getData("posts")}>
+                  Reset Posts
+                </Button>
+              </Grid>
+              <Grid item>
+                <Button
+                  color="secondary"
+                  variant="outlined"
+                  onClick={handleAdd}
+                >
+                  Add Post
+                </Button>
+              </Grid>
+            </Grid>
+          </Grid>
+
+          <TableContainer component={Paper}>
+            <Table sx={{ maxWidth: "90%" }} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Post Id</TableCell>
+                  <TableCell>User Id</TableCell>
+                  <TableCell>Title</TableCell>
+                  <TableCell>Body</TableCell>
+                  <TableCell></TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {returnData.map((row) => (
+                  <TableRow
+                    hover
+                    key={row.id}
+                    sx={{
+                      "&:last-child td, &:last-child th": { border: 0 },
+                    }}
+                  >
+                    <TableCell component="th" scope="row">
+                      {row.id}
+                    </TableCell>
+                    <TableCell>{row.userId}</TableCell>
+                    <TableCell>{row.title}</TableCell>
+                    <TableCell>{row.body}</TableCell>
+                    <TableCell onClick={() => handleUpdate(row)}>
+                      <ModeEditOutlineOutlinedIcon
+                        fontSize="large"
+                        sx={{ color: "blue" }}
+                      />
+                    </TableCell>
+                    <TableCell onClick={() => handleRemove(row)}>
+                      <DeleteForeverIcon
+                        fontSize="large"
+                        sx={{ color: "red" }}
+                      />
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+
+          <Copyright />
         </>
       )}
     </>
---
+++ src/components/useData.js
@@ -0,0 +1,102 @@
+import React, { useState, useEffect } from "react";
+import http from "./services/http-axios";
+
+export default function useData(url) {
+  const [isLoading, setIsLoading] = useState(false);
+  const [returnData, setReturnData] = useState([]);
+  const [error, setServerError] = useState(null);
+
+  useEffect(() => {
+    if (!url) return;
+
+    const fetchData = async () => {
+      getData(url);
+    };
+
+    fetchData();
+  }, [url]);
+
+  const getData = async (url) => {
+    setIsLoading(true);
+    const response = await http
+      .get(url)
+      .then((response) => {
+        setReturnData(response.data);
+      })
+      .catch((error) => {
+        setServerError(error);
+      })
+      .finally(() => setIsLoading(false));
+  };
+
+  const createData = async (url, post) => {
+    if (!url) return;
+    setIsLoading(true);
+    const response = await http
+      .post(url, post)
+      .then((response) => {
+        let updatedData = [...returnData];
+        let newPost = { ...response.data };
+        newPost.id = Math.max(...returnData.map((d) => d.id)) + 1;
+
+        updatedData.push(newPost);
+        setReturnData(updatedData);
+      })
+      .catch((error) => {
+        setServerError(error);
+      })
+      .finally(() => setIsLoading(false));
+  };
+
+  const updateData = async (url, post) => {
+    if (!url) return;
+    setIsLoading(true);
+    const response = await http
+      .put(url, post)
+      .then((response) => {
+        // Simulating an update
+        let originalPostIndex = returnData.findIndex(
+          (obj) => obj.id == post.id
+        );
+        returnData[
+          originalPostIndex
+        ].title = `${returnData[originalPostIndex].title} - Updated`;
+        returnData[
+          originalPostIndex
+        ].body = `${returnData[originalPostIndex].body} - Updated`;
+
+        setReturnData(returnData);
+      })
+      .catch((error) => {
+        setServerError(error);
+      })
+      .finally(() => setIsLoading(false));
+  };
+
+  const deleteData = async (url, postId) => {
+    if (!url) return;
+    setIsLoading(true);
+
+    const response = await http
+      .delete(url)
+      .then((response) => {
+        // Simulate the call which would remove data from the database
+        var filteredArray = returnData.filter((e) => e.id !== postId);
+        setReturnData(filteredArray);
+      })
+      .catch((error) => {
+        setServerError(error);
+      })
+      .finally(() => setIsLoading(false));
+  };
+
+  return {
+    isLoading,
+    returnData,
+    error,
+    getData,
+    createData,
+    updateData,
+    deleteData,
+  };
+}
